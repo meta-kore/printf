44f6d411689d6c64275d0c5aa925b0a86722b0f3
nytah <onwubuyabenita@gmail.com>
2022-07-15 20:30:19 +0100

    wip

--- main.h
+++ main.h
@@ -1,11 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-/**
- * Prototypes of fnctions used 
- */
 
 int _putchar(char c);
 int _printf(char *,...); 
+
