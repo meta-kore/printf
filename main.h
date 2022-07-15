7fea9f7b31a8772666abfd88b7b51ac82169dc52
meta-kore <79240412+meta-kore@users.noreply.github.com>
2022-07-15 11:37:30 -0700

    adding function manager

--- main.h
+++ main.h
@@ -7,5 +7,10 @@
 int _putchar(char c);
 int _printf(char *,...);
 int _printf(char* format,...);
+int loop_format(va_list arg, const char *string)
+int call_function_manager(char aux, va_list arg);
+int check_percent(int *flag, char aux);
+int function_manager(char c, va_list arg);
+int _switch(char c, va_list arg)
 
 #endif
